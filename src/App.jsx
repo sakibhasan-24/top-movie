33ac7fd61f02371cf79f09f4cbe9beb4a8c7f0a0
sakibhasan-24 <sakibhasan7724@gmail.com>
2023-10-20 12:20:46 +0600

    header part

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h3>HEy</h3>
+      <Header />
+    </div>
+  );
 }
 
-export default App
+export default App;
+
+function Header() {
+  return (
+    <header className="header">
+      <nav className="nav ">
+        <ul>
+          <li>Top</li>
+        </ul>
+        <ul className="type-nav">
+          <li>Comedy</li>
+          <li>SIFI</li>
+          <li>Thriller</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+function Movies() {}
+function Footer() {}
